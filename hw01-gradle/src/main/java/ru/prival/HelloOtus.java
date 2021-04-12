196be389b5873cc1e87d888ad5b83eabbc48c24b
Pr1val <pr1val@mail.ru>
2021-04-12 19:38:16 +0300

    новый модуль и класс для домашки №1

---
+++ hw01-gradle/src/main/java/ru/prival/HelloOtus.java
@@ -0,0 +1,24 @@
+package ru.prival;
+
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Objects.equal;
+
+public class HelloOtus {
+    public static void main(String... args) {
+        List<Integer> fibonachi = new ArrayList<>();
+
+        int prev = 1;
+        int current = 1;
+        int amount = 30;
+        for (int i = 1; i <= amount; i++) {
+            fibonachi.add(current);
+            if (i > 2) prev = current - prev;
+            current = prev + current;
+
+        }
+        System.out.println(Lists.reverse(fibonachi));
+    }
+}
